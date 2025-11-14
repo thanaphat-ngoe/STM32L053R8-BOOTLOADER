8e5777adbcc4ae5e22353517e275f45d0fd8caf4
Thanaphat Ngoennet <thanaphat.ngoe@gmail.com>
2025-11-15 01:03:03 +0700

    feature: Implement Ring-Buffer

--- shared/inc/core/ring-buffer.h
+++ shared/inc/core/ring-buffer.h
@@ -11,8 +11,8 @@ typedef struct ring_buffer_t {
 } ring_buffer_t;
 
 void ring_buffer_setup(ring_buffer_t* rb, uint8_t* buffer, uint32_t size);
-void ring_buffer_empty(ring_buffer_t* rb);
-void ring_buffer_write(ring_buffer_t* rb, uint8_t byte);
-void ring_buffer_read(ring_buffer_t* rb, uint8_t* byte);
+bool ring_buffer_empty(ring_buffer_t* rb);
+bool ring_buffer_write(ring_buffer_t* rb, uint8_t byte);
+bool ring_buffer_read(ring_buffer_t* rb, uint8_t* byte);
 
 #endif
